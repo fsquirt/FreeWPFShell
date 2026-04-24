0a06f796a2ad1979009fe53c5ab786de5ba9f00f
关小雨 <guanxiaoyu@cloudyou.top>
2026-04-25 00:31:07 +0800

    更新版本号为1.2.0.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
 [assembly: AssemblyDescription("开源免费的Windows SSH客户端")] // 这项通常不会显示在资源管理器里
 [assembly: AssemblyProduct("FreeWPFShell")]            // 产品名称
 [assembly: AssemblyCopyright("https://github.com/fsquirt")] // 版权
-[assembly: AssemblyVersion("1.1.0.0")]                 // 程序集版本
-[assembly: AssemblyFileVersion("1.1.0.0")]             // 文件版本
+[assembly: AssemblyVersion("1.2.0.0")]                 // 程序集版本
+[assembly: AssemblyFileVersion("1.2.0.0")]             // 文件版本
